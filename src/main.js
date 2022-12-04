ace59b50105c6d96815f182855aea3ea0deb935f
Douglas Lockwood <doug@lockwood-family.com>
2022-12-04 11:00:26 -0700

    Finished System Tray

--- src/main.js
+++ src/main.js
@@ -1,95 +1,16 @@
-const {app, BrowserWindow, Menu, MenuItem} = require('electron')
-const url = require('url')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
 
-let win
+let win;
 
 function createWindow() {
-   win = new BrowserWindow({width: 800, height: 600})
+   win = new BrowserWindow({width: 800, height: 600});
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'pages', 'index.html'),
       protocol: 'file:',
       slashes: true
-   }))
+   }));
 }
 
-const template = [
-   {
-      label: 'Edit',
-      submenu: [
-         {
-            role: 'undo'
-         },
-         {
-            role: 'redo'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'cut'
-         },
-         {
-            role: 'copy'
-         },
-         {
-            role: 'paste'
-         }
-      ]
-   },
-   
-   {
-      label: 'View',
-      submenu: [
-         {
-            role: 'reload'
-         },
-         {
-            role: 'toggledevtools'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'resetzoom'
-         },
-         {
-            role: 'zoomin'
-         },
-         {
-            role: 'zoomout'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'togglefullscreen'
-         }
-      ]
-   },
-   
-   {
-      role: 'window',
-      submenu: [
-         {
-            role: 'minimize'
-         },
-         {
-            role: 'close'
-         }
-      ]
-   },
-   
-   {
-      role: 'help',
-      submenu: [
-         {
-            label: 'Learn More'
-         }
-      ]
-   }
-]
-
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
-app.on('ready', createWindow)
+app.on('ready', createWindow);
